b56941109606a3f9abbd947a40750a55b5b11bc2
Akira Hayashi <smxshxishxad@me.com>
2022-09-03 20:08:32 +0900

    :sparkles: Add type `Inventory` and its methods

---
+++ models/inventory.go
@@ -0,0 +1,62 @@
+package models
+
+type Inventory struct {
+	guitars []*Guitar
+}
+
+func (i *Inventory) AddGuitar(
+	serialNumber string,
+	price float32,
+	builder string,
+	model string,
+	gtype string,
+	backWood string,
+	topWood string,
+) {
+	g := &Guitar{
+		SerialNumber: serialNumber,
+		Price:        price,
+		Builder:      builder,
+		Model:        model,
+		Gtype:        gtype,
+		BackWood:     backWood,
+		TopWood:      topWood,
+	}
+
+	i.guitars = append(i.guitars, g)
+}
+
+func (i *Inventory) getGuitar(serialNumber string) *Guitar {
+	for _, g := range i.guitars {
+		if g.SerialNumber == serialNumber {
+			return g
+		}
+	}
+	return nil
+}
+
+func (i *Inventory) Search(searchGuitar Guitar) *Guitar {
+	for _, g := range i.guitars {
+		if builder := searchGuitar.getBuilder(); (builder != "") && (builder != g.getBuilder()) {
+			continue
+		}
+
+		if model := searchGuitar.getModel(); (model != "") && (model != g.getModel()) {
+			continue
+		}
+
+		if gtype := searchGuitar.getGtype(); (gtype != "") && (gtype != g.getGtype()) {
+			continue
+		}
+
+		if backWood := searchGuitar.getBackWood(); (backWood != "") && (backWood != g.getBackWood()) {
+			continue
+		}
+
+		if topWood := searchGuitar.getBackWood(); (topWood != "") && (topWood != g.getBackWood()) {
+			continue
+		}
+		return g
+	}
+	return nil
+}
